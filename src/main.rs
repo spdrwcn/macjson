1e2294b833345c3ce8b44d8e698696b598da6a16
本子画师三岁半 <157886909+spdrwcn@users.noreply.github.com>
2024-05-26 21:50:47 +0800

    1.00

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use simple_redis;
-use std::collections::HashMap;  
-use std::fs::File;  
+use indicatif::{ProgressBar, ProgressStyle};
+use redis::Commands;
+use std::collections::HashMap;
+use std::fs::File;
 use std::io::Write;
-use serde::Deserialize; 
-use serde::Serialize;  
-use serde_json::to_string_pretty;  
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::to_string_pretty;
 use clap::{App as ClapApp, Arg};
 
-  
-fn main() -> Result<(), Box<dyn std::error::Error>> {  
+#[derive(Serialize, Deserialize, Debug)]
+struct DeviceInfo {
+    bluetooth_mac: String,
+    wired_mac: String,
+    wireless_mac: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = ClapApp::new("macjson")
         .version("1.0.0")
         .author("h13317136163@163.com")
@@ -23,23 +30,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .get_matches();
     let ip_address = matches.value_of("ip").unwrap();
-    let mut redis = simple_redis::create(ip_address)?;
-    let keys: Vec<String> = redis.keys("*")?;  
-    let mut key_value_pairs: HashMap<String, DeviceInfo> = HashMap::new();   
-    for key in keys {  
-        let value: String = redis.get(&key)?;
-        let device_info: DeviceInfo = serde_json::from_str(&value)?;  
-        key_value_pairs.insert(key, device_info);  
-    }  
-    let json_string = to_string_pretty(&key_value_pairs)?;  
-    let mut file = File::create("mac.json")?;  
-    file.write_all(json_string.as_bytes())?;  
-    Ok(())  
-}  
+    let client = redis::Client::open(ip_address)?;
+    let mut con = client.get_connection()?;
+    let keys: Vec<String> = con.keys("*")?;
 
-#[derive(Serialize, Deserialize, Debug)]  
-struct DeviceInfo {  
-    bluetooth_mac: String,  
-    wired_mac: String,  
-    wireless_mac: String,  
-}  
+    let pb = ProgressBar::new(keys.len() as u64);
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template(
+                "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})",
+            )?
+            .progress_chars("#>-"),
+    );
+
+    let mut key_value_pairs: HashMap<String, DeviceInfo> = HashMap::new();
+    for (_index, key) in keys.iter().enumerate() {
+        pb.inc(1);
+        let value: String = con.get(key)?;
+        let device_info: DeviceInfo = serde_json::from_str(&value)?;
+        key_value_pairs.insert(key.to_owned(), device_info);
+    }
+
+    pb.finish_with_message("All keys processed.");
+    let json_string = to_string_pretty(&key_value_pairs)?;
+    let mut file = File::create("mac.json")?;
+    file.write_all(json_string.as_bytes())?;
+
+    Ok(())
+}
